db184a90f779d5f44036854c6cc52dc8bc79c58b
VDAUTREMENT <valentin.dautrement@ynov.com>
2022-04-26 13:59:57 +0000

    add login viewmodel

--- App/Data/AppDbContextExtensions.cs
+++ App/Data/AppDbContextExtensions.cs
@@ -9,11 +9,48 @@ namespace App.Data
             if (dbContext.ResultItems.Any()){
 
             }
-            var resultItems = new List<ResultItem>(){
+            // var Races = new List<Race>()
+            // {
+            //     new Race()
+            //     {
+            //         Id = 1,
+            //         Name = "Race 1",
+            //         EventDate = new DateTime(2022, 04, 20, 15, 45, 00),
+            //         StartHour = 15,
+            //         Latitude = 15,
+            //         Longitude = -60,
+            //         MaxParticipants = 10,
+            //         Image = "https://via.placeholder.com/150",
+            //         AgeRestriction = 18,
+            //     },
+            //     new Race()
+            //     {
+            //         Id = 2,
+            //         Name = "Race 2",
+            //         EventDate = new DateTime(2022, 05, 02, 13, 30, 00),
+            //         StartHour = 13,
+            //         Latitude = 15,
+            //         Longitude = -60,
+            //         MaxParticipants = 10,
+            //         Image = "https://via.placeholder.com/150",
+            //         AgeRestriction = 18,
+            //     },
+            //     new Race()
+            //     {
+            //         Id = 3,
+            //         Name = "Race 3",
+            //         EventDate = new DateTime(2022, 06, 02, 13, 40, 00),
+            //         StartHour = 13,
+            //         Latitude = 40,
+            //         Longitude = -20,
+            //         MaxParticipants = 15,
+            //         Image = "https://via.placeholder.com/150",
+            //         AgeRestriction = 16,
+            //     },
 
-            };
-            dbContext.ResultItems.AddRange(resultItems);
-            dbContext.SaveChanges();
+            // };
+            // dbContext.Races.AddRange(Races);
+            // dbContext.SaveChanges();
         }
     }
 }
